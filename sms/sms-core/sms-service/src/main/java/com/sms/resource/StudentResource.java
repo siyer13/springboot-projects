8c199e7cbbcd2bb5111e511eea761d9e2ae6cba0
Sridhar <sreedharriyer@gmail.com>
2018-12-31 17:52:58 +0530

    School Management Application  - Gradle Multi Module project

--- sms/sms-core/sms-service/src/main/java/com/sms/resource/StudentResource.java
+++ sms/sms-core/sms-service/src/main/java/com/sms/resource/StudentResource.java
@@ -3,8 +3,11 @@ package com.sms.resource;
 import com.sms.model.Student;
 import com.sms.service.StudentService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.Authorization;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -12,10 +15,16 @@ import java.util.List;
 @RequestMapping("/student")
 public class StudentResource implements StudentService {
 
+    @Autowired
+    Student student;
+
     @Override
     @GetMapping("/getAllStudents")
     @ResponseBody
     public List<Student> getStudentList() {
-        return null;
+        List<Student> studentList = new ArrayList<>();
+        student.setStudentName("Sridhar");
+        studentList.add(student);
+        return studentList;
     }
 }
