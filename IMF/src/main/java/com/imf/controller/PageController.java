7529fd43808c1e3a15477218784e03a1748a9196
Sridhar <sreedharriyer@gmail.com>
2019-01-18 21:42:35 +0530

    Some more changes to test

--- IMF/src/main/java/com/imf/controller/PageController.java
+++ IMF/src/main/java/com/imf/controller/PageController.java
@@ -3,6 +3,8 @@ package com.imf.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -15,4 +17,17 @@ public class PageController {
 		return new ModelAndView("index");
 	}
 
+	@PostMapping("/someotherpage")
+	public ModelAndView ShomePage(@RequestParam("name") String name, Model model) {
+		model.addAttribute("title", "Mutual Funds of India");
+		model.addAttribute("message", name + "hahaha...This is my second sping mvc page");
+		return new ModelAndView("jumbo");
+	}
+	
+	@GetMapping("/linkpage")
+	public ModelAndView linkPage(Model model) {
+		model.addAttribute("title", "Mutual Funds of India");
+		model.addAttribute("message", "Hey the link page got clicked");
+		return new ModelAndView("jumbo");
+	}
 }
--- IMF/src/main/resources/templates/index.ftl
+++ IMF/src/main/resources/templates/index.ftl
@@ -7,8 +7,20 @@
         <link href="css/app.css" rel="stylesheet"/>
     </head>
     <body>
- 
         <div ui-view></div>
      ${message}
+     
+     <div class="form">
+    <form action="someotherpage" method="post" onsubmit="true">
+      <table>
+        <tr>
+          <td>Enter Your name</td>
+          <td><input id="name" name="name"></td>
+          <td><input type="submit" value="Submit"></td>
+          <td><a href="linkpage">Link Page</a>
+        </tr>
+      </table>
+    </form>
+  </div>
     </body>
 </html>
