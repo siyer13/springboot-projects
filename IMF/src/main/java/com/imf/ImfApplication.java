8fc20407b17921443afe18d86c4d475efdf960d0
Sridhar <sreedharriyer@gmail.com>
2019-01-18 14:08:35 +0530

    IMF2- Main Application

---
+++ IMF/src/main/java/com/imf/ImfApplication.java
@@ -0,0 +1,14 @@
+package com.imf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImfApplication.class, args);
+	}
+
+}
+
