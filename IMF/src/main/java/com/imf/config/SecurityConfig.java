d6b57639dcd4696b7682b45008e01d5bb8f50603
Sridhar <sreedharriyer@gmail.com>
2019-02-03 17:45:03 +0530

    Added spring security - Working

--- IMF/src/main/java/com/imf/config/SecurityConfig.java
+++ IMF/src/main/java/com/imf/config/SecurityConfig.java
@@ -1,34 +1,45 @@
 package com.imf.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	@Override
-	public void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-		.authorizeRequests()
-		.anyRequest()
-		.authenticated()
-		.and()
-		.httpBasic();
-	}
-	
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		.withUser("admin")
-		.password("{noop}password")
-		.roles("USER");
-	}
-	
-	
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception 
+    {
+    	http
+        .authorizeRequests()
+        .antMatchers("/**").authenticated() // These urls are allowed by any authenticated user
+        .and()
+        .httpBasic();
+http.csrf().disable();
+    }
+ 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // Get the user credentials from the console (or any other source):
+        String username = "hans";
+        String password = "hans";
 
+        // Set the inMemoryAuthentication object with the given credentials:
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        String encodedPassword = passwordEncoder().encode(password);
+        manager.createUser(User.withUsername(username).password(encodedPassword).roles("USER").build());
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
